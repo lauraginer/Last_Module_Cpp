c3ccccaca919dfefc45ead26d41cddc523fedbf3
lauraginer <lauginmat@gmail.com>
2026-03-05 19:52:21 +0100

    Update, need to do a real class and need to be fast with this module!! good luck :)

--- CPP_05/ex03/Intern.cpp
+++ CPP_05/ex03/Intern.cpp
@@ -6,7 +6,36 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 16:56:30 by lginer-m          #+#    #+#             */
-/*   Updated: 2026/03/02 16:56:31 by lginer-m         ###   ########.fr       */
+/*   Updated: 2026/03/05 13:34:43 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(): AForm("Presidential Pardon", 25, 5), target("Default")
+{
+	std::cout << "Default constructor of PresidentialPardonForm called\n";
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &subject): AForm("Presidential Pardon", 25, 5), target(subject)
+{
+	std::cout << "Parametrized constructor of PresidentialPardonForm called\n";
+}
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other): AForm(other), target(other.target){
+	std::cout << "Copy constructor of PresidentialPardonForm called\n";
+}
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other)
+{
+	std::cout << "Copy assignment operator of PresidentialPardonForm called\n";
+	if(this != &other)
+		AForm::operator=(other);
+	return(*this);
+}
+PresidentialPardonForm:: ~PresidentialPardonForm()
+{
+	std::cout << "Destructor of PresidentialPardonForm called\n";
+}
+void PresidentialPardonForm::executeAction() const
+{
+	std::cout << target << " has been pardoned by Zaphod Beeblebrox\n";
+}
