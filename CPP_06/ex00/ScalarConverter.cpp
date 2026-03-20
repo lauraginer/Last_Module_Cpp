f117e73208a71994fc32ac6d586face29ddf319c
lauraginer <lauginmat@gmail.com>
2026-03-20 20:10:52 +0100

    update, this explotes my head. Read the header and comments because is crazy. Need to parser everything!!

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -6,12 +6,13 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 16:36:36 by lginer-m          #+#    #+#             */
-/*   Updated: 2026/03/19 16:00:50 by lginer-m         ###   ########.fr       */
+/*   Updated: 2026/03/20 20:08:48 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <cctype>
+#include <cmath>
 
 ScalarConverter::ScalarConverter()
 {
@@ -33,25 +34,57 @@ ScalarConverter::~ScalarConverter()
 {
 	std::cout << "Destructor of ScalarConverter called\n";
 }
-static void convert(std::string literal)
+void ScalarConverter::convert(std::string literal)
 {
-	char c = literal[0];
+	if(literal.empty())
+		throw ScalarConverter::GeneralError();
+	if (literal.size() == 1)
+		char c = literal[0];
+	/*if (literal.size() > 1 && literal.size() < 4 && isprint())
+		// hace un atoi o lo que sea
+	else
+		// not printable
+	vale, en funcion de la longitud del string el parseo, antes que nada, y en ello hay que comprobar los limites
+	y te quitas una parte importante
+	*/
 	int n = static_cast<int>(c);
-	float fnum;
-	double dnum;
+	float fnum = static_cast<float>(c);
+	double dnum = static_cast<double>(c);
 	//Detectar tipo, convertir, imprimir resultados
- 	if(literal.empty() || !(isprint(c)))
-		throw ScalarConverter::GeneralError();//comprobar si es imprimible, utilizar la biblioteca cctype CORRIGELO
-	
-	//debería de aceptar espacios digo yo no?(son imprimibles)
-	
-
-}
-const char* ScalarConverter::CanNotPrint::what() const throw()
-{
-	return("Error. This result is not a printable ASCII character.");
+	if(controlType(c, n, fnum, dnum))
+	{
+		std::cout << "char: " << c << std::endl;
+		std::cout << "int: " << n << std::endl;
+		std::cout << "float: " << fnum << std::endl;
+		std::cout << "double: " << dnum << std::endl;
+	}
 }
 const char* ScalarConverter::GeneralError::what() const throw()
 {
 	return("Process of convert isn't valid. Wrong input.");
+}
+bool controlType(char c, int n, float fnum, double dnum)
+{
+	bool flag;
+		
+	flag = true;
+	if(std::isnan(dnum) || std::isinf(dnum)) // nan = not a number, inf = infinito
+	{
+		flag = false;
+		std::cout << "char: Impossible" << std::endl;
+		std::cout << "int: Impossible" << n << std::endl;
+	}	
+	else if(!(isprint(c)) || c < 32 || c > 127)
+	{
+		flag = false;
+		std::cout << "char: Non displayable" << std::endl;
+		std::cout << "int: " << n << std::endl;
+	}
+	(void)fnum;
+	(void)dnum;
+	return(flag);
+	//tengo una idea, utilizar el switch y segun que casos comprobar char, int, float y tal
+	//sino, es una puta locura
+
+	//NO SWITCH NO, ES MUCHISIMO PEOR
 }
--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 16:25:49 by lginer-m          #+#    #+#             */
-/*   Updated: 2026/03/12 16:25:50 by lginer-m         ###   ########.fr       */
+/*   Updated: 2026/03/20 20:00:42 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		std::cout << "Wrong input. Please insert one argument!\n";
+		return (1);
+	}
+	else{
+		ScalarConverter::convert(argv[1]);
+		std::string literal = argv[1];
+		std::cout<<"longitud: "<< literal.size() << std::endl;
+	}
+	return (0);
+}
