2e83e571e87427679d8746dd9e62b67ae7337f40
lauraginer <lauginmat@gmail.com>
2026-04-27 14:45:08 +0200

    A few comments in main about what i need to do

--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 12:36:49 by lauragm           #+#    #+#             */
-/*   Updated: 2026/04/27 13:13:18 by lginer-m         ###   ########.fr       */
+/*   Updated: 2026/04/27 14:42:41 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int main()
 {
-	
+	//probar con int * a = new int();` y luego muestre *a. Ejemplo de main.
+	//probar con char y double o float a ver si soporta
+	//probar con constructor de copia y asignacion
+	//probar con casos que esten mal (al estilo un indice fuera de rango) blablabla
+
+	//NO OLVIDES QUE TODO DEBE DE IR DENTRO DE UNA EXCEPCIÓN
 }
