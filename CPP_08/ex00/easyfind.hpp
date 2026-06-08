bbef96e356f51d6030b627a5679a5acf6f365c43
Laura Giner <lauginmat@gmail.com>
2026-06-08 21:49:34 +0200

    Ok. I see a important concept of 7 module that maybe it will change the last correction, but anyways. I've created the new module with some comments of the porpouse of this cpp, need to study a lot of things, step to step.

---
+++ CPP_08/ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/08 21:00:13 by lauragm           #+#    #+#             */
+/*   Updated: 2026/06/08 21:44:16 by lauragm          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+//crear una funcion de tipo template que acepte un tipo T. Tiene que aceptar como parametros, 
+//el 1º un contenedor de tipo T por ejemplo (std::vector<int> numbers), el 2º un entero
+
+//hay que buscar la primera aparición del entero dentro del contenedor. Si este falla, hay que lanzar una excepción y si no,
+//devolver algún valor de error
+
+#endif
+
+
+/*Buscar contenedores asociativos, buscar que cojones es stl, que se diferencia un vector de un contenedor (y que es este último) y afianzar conceptos basicos
+que se me han olvidado por no programar en un mes y pico lista que de lista eres gilipollas*/
+
+//añade toda la mierda explicativa a explication.txt
