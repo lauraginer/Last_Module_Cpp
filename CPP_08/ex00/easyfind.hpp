21e5fac7d85bc4c082bd615ef33ea0b1e4b5fe41
Laura Giner <lauginmat@gmail.com>
2026-06-09 21:32:26 +0200

    Update. Need to implement now the function with a few different types of containers. Good luck.

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,16 +6,20 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 21:00:13 by lauragm           #+#    #+#             */
-/*   Updated: 2026/06/08 21:44:16 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/06/09 21:10:07 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+template <typename T>
+typename T::iterator easyfind(T& container, int value) //recuerda que los templates siempre tienen que ir en el compilador para generar una version concreta para cada tipo
+{
+	//el subject dice que la función debe servir para CUALQUIER contenedor, el template se encarga de que funcione con todos.
+}
 //crear una funcion de tipo template que acepte un tipo T. Tiene que aceptar como parametros, 
 //el 1º un contenedor de tipo T por ejemplo (std::vector<int> numbers), el 2º un entero
-
 //hay que buscar la primera aparición del entero dentro del contenedor. Si este falla, hay que lanzar una excepción y si no,
 //devolver algún valor de error
 
