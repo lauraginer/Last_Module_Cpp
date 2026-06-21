edc576ed0f8ced06d20bf2c2f6c6e7eab37dd69b
Laura Giner <lauginmat@gmail.com>
2026-06-21 22:04:46 +0200

    a little modification hehe

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 21:00:13 by lauragm           #+#    #+#             */
-/*   Updated: 2026/06/21 21:58:40 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/06/21 22:04:24 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,6 @@ typename T::iterator easyfind(T& container, int value) //recuerda que los templa
 		++it; //fumadon historico: es lo mismo que it++, solo que el que utilizamos siempre crea una copia temporal al parecer y devuelve el valor antes de incrementarlo
 	}
 	throw errorToFind();
-}
+};
 //el subject dice que la función debe servir para CUALQUIER contenedor, el template se encarga de que funcione con todos.
 #endif
