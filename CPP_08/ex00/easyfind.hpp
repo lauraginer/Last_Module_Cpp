11a81e3cabdb89b1d39a4b106a6c7a50e8379b38
Laura Giner <lauginmat@gmail.com>
2026-06-21 22:03:40 +0200

    The first exercise is done! Need to prove with more types of containers, but it seems that works good! Keep going :)

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,27 +6,35 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 21:00:13 by lauragm           #+#    #+#             */
-/*   Updated: 2026/06/09 21:10:07 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/06/21 21:58:40 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <exception>
+#include <iostream>
+
+class errorToFind: public std::exception{
+	public:
+		const char *what() const throw()
+		{
+			return("Error. Doesn't find the value or is empty!");
+		}
+};
+
 template <typename T>
 typename T::iterator easyfind(T& container, int value) //recuerda que los templates siempre tienen que ir en el compilador para generar una version concreta para cada tipo
 {
-	//el subject dice que la función debe servir para CUALQUIER contenedor, el template se encarga de que funcione con todos.
+	typename T::iterator it = container.begin();
+	while(it != container.end())
+	{
+		if(*it == value)
+			return(it);
+		++it; //fumadon historico: es lo mismo que it++, solo que el que utilizamos siempre crea una copia temporal al parecer y devuelve el valor antes de incrementarlo
+	}
+	throw errorToFind();
 }
-//crear una funcion de tipo template que acepte un tipo T. Tiene que aceptar como parametros, 
-//el 1º un contenedor de tipo T por ejemplo (std::vector<int> numbers), el 2º un entero
-//hay que buscar la primera aparición del entero dentro del contenedor. Si este falla, hay que lanzar una excepción y si no,
-//devolver algún valor de error
-
+//el subject dice que la función debe servir para CUALQUIER contenedor, el template se encarga de que funcione con todos.
 #endif
-
-
-/*Buscar contenedores asociativos, buscar que cojones es stl, que se diferencia un vector de un contenedor (y que es este último) y afianzar conceptos basicos
-que se me han olvidado por no programar en un mes y pico lista que de lista eres gilipollas*/
-
-//añade toda la mierda explicativa a explication.txt
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,7 +6,30 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 21:00:19 by lauragm           #+#    #+#             */
-/*   Updated: 2026/06/08 21:00:20 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/06/21 21:57:31 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+#include <vector>
+
+int main()
+{
+	try{
+		std::cout << "--TEST 1: TESTING WITH VECTORS--\n";
+		std::vector<int> numbers;
+		std::vector<int> empty;
+		numbers.push_back(20);
+		numbers.push_back(4);
+		numbers.push_back(8);
+		numbers.push_back(15);
+		std::vector<int>::iterator it = easyfind(numbers, 8); //guardamos el result en otro iterador
+		std::cout << "Found it: " << *it << std::endl;
+		
+		std::vector<int>::iterator emt = easyfind(empty, 0);
+		std::cout << "Found it: " << *emt << std::endl;
+	}
+	catch(const std::exception& e){
+		std::cout << e.what() << std::endl;
+	}
+}
