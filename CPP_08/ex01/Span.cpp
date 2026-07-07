ebb9c0fcb49595518165752675a7585f88f561b6
Laura Giner <lauginmat@gmail.com>
2026-07-07 21:21:12 +0200

    Ok. Need to implement the rest of the functions and add more test to complete the ex02. I wish having more time

--- CPP_08/ex01/Span.cpp
+++ CPP_08/ex01/Span.cpp
@@ -6,34 +6,47 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/05 20:03:28 by lauragm           #+#    #+#             */
-/*   Updated: 2026/07/07 20:26:46 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/07/07 21:19:08 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-Span::Span(): maxSize(0){
+Span::Span(): maxSize(0)
+{
 	std::cout << "Default constructor of Span called\n";
 }
-Span::Span(unsigned int max): maxSize(max){
+Span::Span(unsigned int max): maxSize(max)
+{
 	std::cout << "Parametrized constructor of Span called\n";
 }
-Span::Span(const Span& other): maxSize(other.maxSize){
+Span::Span(const Span& other): maxSize(other.maxSize)
+{
 	std::cout << "Copy constructor of Span called\n";
 }
-Span::Span& operator=(const Span& other){
+Span::Span& operator=(const Span& other)
+{
 	std::cout << "Copy assignment operator of Span called\n";
 	if(this != &other)
-		maxSize = other.maxSize
-	return(*this)
+		maxSize = other.maxSize;
+	return(*this);
 }
-Span::~Span(){
+Span::~Span()
+{
 	std::cout << "Destructor of Span called\n";
 }
-void Span::addNumber(unsigned int number){
-	
+void Span::addNumber(unsigned int number)
+{
+	if(numbers.size() >= maxSize)
+		throw SpanError();
+	numbers.push_back(number);
 }
+unsigned int Span::longestSpan()
+{
+	if(!numbers.size() || numbers.size() == 1)
+		throw SpanError();
 
+}
 const char* SpanError::what() const throw()
 {
 	return("Error. Something failed on Span");
