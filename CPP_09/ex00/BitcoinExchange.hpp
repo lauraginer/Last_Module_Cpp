51421121f96de8c3bfa8471565b610e3b56eacef
Laura Giner <lauginmat@gmail.com>
2026-08-18 21:43:04 +0200

    I add a few important comments and the missing files. good luck!

--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 20:02:19 by lauragm           #+#    #+#             */
-/*   Updated: 2026/08/18 21:04:05 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/08/18 21:32:58 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,13 @@
 
 class BitcoinExchange{
 	private:
-	
+	//aqui iria un vector tipo map
 	public:
 		BitcoinExchange();
   		BitcoinExchange(const BitcoinExchange& other);
     	BitcoinExchange& operator=(const BitcoinExchange& other);
 		~BitcoinExchange();
-	
 }
-
 class errorArgument: public std::exception{
 	public:
 		const char *what() const throw();
@@ -34,10 +32,10 @@ class errorArgument: public std::exception{
 class errorToDate: public std::exception{
 	public:
 		const char *what() const throw();
-};
+}; //por ahora, aunque es mejor condicionales para la cantidad de errores que hay que manejar
 
 //necesitamos un segundo argumento que sea un archivo.txt
 //Cada línea de este argumento debe de ser date|value
 //Una fecha valida debe se ser Año-Mes-Día como losputos guiris
 //Un valor valido deberia de ser un float o un valor positivo, entre 0 y 1000
-//Tenemos que utilizar un contenedor, y un manejar mensajes de errores apropiados al contexto
+//Tenemos que utilizar un contenedor, y manejar mensajes de errores apropiados al contexto
