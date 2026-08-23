1be623b9defdb4ce3d898e83af6c4cc2e5fd1005
Laura Giner <lauginmat@gmail.com>
2026-08-23 20:56:24 +0200

    Need to follow with the parser. Check comments and control the spaces!

--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -6,12 +6,25 @@
 /*   By: lauragm <lauragm@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 20:02:19 by lauragm           #+#    #+#             */
-/*   Updated: 2026/08/20 22:07:05 by lauragm          ###   ########.fr       */
+/*   Updated: 2026/08/23 20:55:30 by lauragm          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 #include <fstream> //leer archivos
+
+int parserValues(std::string line, size_t pos)
+{
+	std::string date;
+	std::string value;
+	date = line.substr(0, pos);
+	value = line.substr(pos + 1);
+	std::cout << date << std::endl;
+	std::cout << value << std::endl;
+	return(0);
+	//hay que separar espacios
+};
+
 int main(int argc, char **argv)
 {
 	try{
@@ -21,9 +34,15 @@ int main(int argc, char **argv)
 		if(!file.is_open())
 			throw errorArgument();
 		
-		std::string line;
-		while(std::getline(file, line)) //lee los caracteres de file y los guarda todo en line
+		std::string line; 
+		std::getline(file, line);//1 llamada para saltarnos el enunciado
+		while(std::getline(file, line)) 
 		{
+			size_t posLine = line.find('|');
+			if(posLine == std::string::npos) //npos es lo que devuelve find cuando no encuentra el pos
+				std::cout << "Error: bad input => " << line << std::endl;
+			if(parserValues(line, posLine))
+				std::cout << "Error: Fail to get values" << std::endl; //solo para comprobar
 			//aqui hacemos todo el parser
 			//parseamos la línea (separar fecha y valor, convertirla en float y blabla)
 			//paseamos la fecha, formato valido
@@ -35,3 +54,4 @@ int main(int argc, char **argv)
 		std::cout << e.what() << std::endl;
 	}
 }
+
